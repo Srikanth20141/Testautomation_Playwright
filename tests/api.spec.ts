9f4ee99dbc76792d3bcaa39bc20a66598b042efb
Srikanth Sathinathan <srikanth.sathinathan@criticalsoftware.com>
2024-04-25 23:03:14 +0100

    authentications

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -7,6 +7,23 @@ test('Get request', async ({ request }) => {
     console.log(body);
     /* status have ok */
     expect(responds.ok).toBeTruthy();
-    /* verify the status code */ 
+    /* verify the status code */
     expect(responds.status()).toBe(200);
+});
+
+
+test('Create user', async ({ request }) => {
+    const token = '898d6dc2d36c9c062ea4fa564f019b450c4f24825e3d8c9551d25d0ff5ca7a47'
+    const responds = await request.post('https://gorest.co.in/public/v2/users', {
+        data:
+            { "name": "srikanth", "gender": "male", "email": "sathinathan@gmail.com", "status": "active", },
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const res = await responds.json();
+
+    console.log(res);
+
+    expect(responds.status()).toBe(201);
 })
