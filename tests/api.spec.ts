c50b9d5f45b50643d766851a290783b6c05a233c
Srikanth Sathinathan <srikanth.sathinathan@criticalsoftware.com>
2024-04-26 07:24:49 +0100

    get the api and from previous responds

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -26,12 +26,41 @@ test('Create user', async ({ request }) => {
         }
     });
     const res = await responds.json();
+    const id = res.id;
 
     console.log(res);
 
     expect(responds.status()).toBe(201);
     expect(res).toHaveProperty('name', name);
     expect(res).toHaveProperty('email', email);
+});
+
+test('get a specific user', async ({ request }) => {
+    let name = faker.person.firstName();
+    let email = faker.internet.email();
+    const token = '898d6dc2d36c9c062ea4fa564f019b450c4f24825e3d8c9551d25d0ff5ca7a47';
+    const responds = await request.post('https://gorest.co.in/public/v2/users', {
+        data:
+            { "name": name, "gender": "male", "email": email, "status": "active", },
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const res = await responds.json();
+    const id = res.id;
+
+    console.log(res);
+
+    expect(responds.status()).toBe(201);
+    expect(res).toHaveProperty('name', name);
+    expect(res).toHaveProperty('email', email);
+    const getReq = await request.get(`https://gorest.co.in/public/v2/users/${id}`,{
+        headers:{
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const body = await getReq.json();
+    console.log(body);
 });
 
 test('Create user from the json file', async ({ request }) => {
